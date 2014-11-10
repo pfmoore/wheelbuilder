a5d5af7b7cd1e6589870ac847cc9af330cae1f84
Paul Moore <p.f.moore@gmail.com>
2014-11-10 22:07:01 +0000

    Make a project from PyPI into an object

--- tests/test_pypi.py
+++ tests/test_pypi.py
@@ -1,9 +1,11 @@
-from wheelbuilder import pypi
+from wheelbuilder.pypi import PyPI
 
 def test_getfiles():
-    version, files = pypi.get('pip')
-    assert version == '1.5.6'
+    proj = PyPI('pip')
+    assert proj.version == '1.5.6'
 
 def test_compat():
-    assert pypi.has_compatible_wheel('pip')
-    assert not pypi.has_compatible_wheel('pywin32')
+    proj = PyPI('pip')
+    assert proj.has_compatible_wheel()
+    proj = PyPI('pywin32')
+    assert not proj.has_compatible_wheel()
--- wheelbuilder/pypi.py
+++ wheelbuilder/pypi.py
@@ -1,14 +1,13 @@
 import requests
 from wheel.install import WheelFile
 
-def get(project):
-    url = 'https://pypi.python.org/pypi/{}/json'.format(project)
-    req = requests.get(url)
-    data = req.json()
-    ver = data['info']['version']
-    files = data['releases'][ver]
-    return ver, files
+class PyPI:
+    def __init__(self, project):
+        url = 'https://pypi.python.org/pypi/{}/json'.format(project)
+        req = requests.get(url)
+        data = req.json()
+        self.version = data['info']['version']
+        self.files = data['releases'][self.version]
 
-def has_compatible_wheel(project):
-    ver, files = get(project)
-    return any(WheelFile(obj['filename']).compatible for obj in files)
+    def has_compatible_wheel(self):
+        return any(WheelFile(obj['filename']).compatible for obj in self.files)
