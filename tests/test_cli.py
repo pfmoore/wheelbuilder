c1c08f33f2fb8822662aab4c0b89d3476a72b02a
Paul Moore <p.f.moore@gmail.com>
2014-11-10 22:01:43 +0000

    Added some more command line functionality

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,30 @@
 import click
 from click.testing import CliRunner
-from wheelbuilder import main
+from wheelbuilder import main, __version__
 
 def test_help():
     runner = CliRunner()
     result = runner.invoke(main, ['--help'])
     assert result.exit_code == 0
     assert result.output != ''
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(main, ['--version'])
+    assert result.exit_code == 0
+    assert __version__ in result.output
+
+def test_project_arg():
+    runner = CliRunner()
+    result = runner.invoke(main, ['pip'])
+    assert result.exit_code == 0
+
+def test_multiple_projects():
+    runner = CliRunner()
+    result = runner.invoke(main, ['pip', 'setuptools'])
+    assert result.exit_code == 0
+
+def test_file_arg():
+    runner = CliRunner()
+    result = runner.invoke(main, ['--file', 'projects.txt'])
+    assert result.exit_code == 0
--- wheelbuilder/__init__.py
+++ wheelbuilder/__init__.py
@@ -1,7 +1,11 @@
 import click
 
+__version__ = '0.1'
+
 @click.command()
-@click.option('--project', help='Project to build.')
-def main(project):
+@click.version_option(version=__version__)
+@click.argument('project', nargs=-1)
+@click.option('--file', help="A file containing projects to build")
+def main(project, file):
     """Simple program that builds a wheel for PROJECT."""
     pass
